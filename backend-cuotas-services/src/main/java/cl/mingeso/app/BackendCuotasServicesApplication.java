6e431639e35ee2d6e4fc5a184e12201269839b4b
Altobert <alberto.sanmartin@usach.cl>
2023-10-29 19:05:50 -0300

    commit para crear archivos

---
+++ backend-cuotas-services/src/main/java/cl/mingeso/app/BackendCuotasServicesApplication.java
@@ -0,0 +1,13 @@
+package cl.mingeso.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCuotasServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCuotasServicesApplication.class, args);
+	}
+
+}
---
+++ backend-estudiantes-services/src/main/java/cl/mingeso/app/BackendEstudiantesServicesApplication.java
@@ -0,0 +1,13 @@
+package cl.mingeso.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEstudiantesServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEstudiantesServicesApplication.class, args);
+	}
+
+}
