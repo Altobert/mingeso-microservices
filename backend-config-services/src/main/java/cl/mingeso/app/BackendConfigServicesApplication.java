dfee13db46b2182861fc2d767bc5b793cc178167
Altobert <alberto.sanmartin@usach.cl>
2023-10-29 18:50:16 -0300

    primer commit

---
+++ backend-config-services/src/main/java/cl/mingeso/app/BackendConfigServicesApplication.java
@@ -0,0 +1,13 @@
+package cl.mingeso.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendConfigServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendConfigServicesApplication.class, args);
+	}
+
+}
---
+++ backend-gateway-services/src/main/java/cl/mingeso/app/BackendGatewayServicesApplication.java
@@ -0,0 +1,13 @@
+package cl.mingeso.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGatewayServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGatewayServicesApplication.class, args);
+	}
+
+}
